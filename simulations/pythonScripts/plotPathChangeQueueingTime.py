0862f9641c3f97c8043aca270588f8ffc1065f89
Aiden Valentine <av288@m902074.inf.susx.ac.uk>
2024-07-08 12:49:39 +0100

    Path change experiments with cubic and bbr

---
+++ simulations/pythonScripts/plotPathChangeQueueingTime.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import re
+
+def parse_if_number(s):
+    try: return float(s)
+    except: return True if s=="true" else False if s=="false" else s if s else None
+
+def parse_ndarray(s):
+    return np.fromstring(s, sep=' ') if s else None
+
+def parse(s):
+    return s;
+    
+def getResults(file):
+    resultsFile = pd.read_csv(file, converters = {
+    'module' : parse,
+    'attrvalue': parse_if_number,
+    'binedges': parse_ndarray,
+    'binvalues': parse_ndarray,
+    'vectime': parse_ndarray,
+    'vecvalue': parse_ndarray})
+    vectors = resultsFile[resultsFile.type=='vector']
+    vec = vectors[vectors.name == 'queueingTime:vector']
+    return vec;
+
+if __name__ == "__main__":
+    results = []
+    for arg in sys.argv[1:]:
+        results.append(getResults(arg))
+    i = 0
+    plt.figure(figsize=(25,12))
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    for result in results:
+        colorNum = 0
+        queueName = "simplenetworkpathchange.constantRouter1.ppp["
+        lastQueue = 0
+        moduleList = result.module.to_numpy()
+        extractedList = [s for s in moduleList if queueName in s]
+        for exWord in extractedList:
+            pppNumb = re.search(r"\[([A-Za-z0-9_]+)\]", exWord)
+            if(int(pppNumb.group(1)) > lastQueue):
+                lastQueue = int(pppNumb.group(1))
+        numpyIndex = 0
+        for mod in result.module.to_numpy():
+            if(mod == "simplenetworkpathchange.constantRouter1.ppp[" + str(5) + "]"+".queue"):
+                break
+            numpyIndex = numpyIndex + 1
+            
+        yAxis = result.vecvalue.to_numpy()[numpyIndex]
+        #print(results.vecvalue)
+        plt.plot(result.vectime.to_numpy()[numpyIndex],yAxis*1000
+        , drawstyle='steps-post', linewidth=1)
+        colorNum += 1
+        
+        axes = plt.gca()
+                #axes.set_xlim([5, 5.2])
+        #axes.set_ylim([0,150])
+        plt.xlabel('Time (s)', fontsize=28)
+        plt.ylabel('Queueing Time (ms)', fontsize=28)
+        plt.legend(loc = "upper right")
+        plt.title("Queue Length", fontsize=35)
+        #plt.xticks((np.arange(0, result["Goodput"].idxmax(), step=250)))
+        plt.savefig('QueueingTime.png')
+        i += 1
+    
+
---
+++ simulations/pythonScripts/plotPathChangeResults.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+# Plots results given the OrbTCP flavour folders
+# plotResults orbtcp ... orbTcpFlavourN
+# Aiden Valentine
+
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from pathlib import Path
+import os
+import shutil
+import subprocess
+           
+if __name__ == "__main__":
+    cores = 4
+    folders = ["OneFlow", "TwoFlows", "FiveFlows", "TenFlows", "TwentyfiveFlows"]
+    for arg in sys.argv[1:]:
+        folderLoc =  '../'+ arg +'/results/'
+        print("------------ Plotting results for " + arg + "------------")
+        #clear folders
+        processNestedList = []
+        processFolderNameList = []
+        for fi in folders:
+            dir = '../pythonResults/' + arg + '/' + fi
+            if os.path.exists(dir):
+                shutil.rmtree(dir)
+            os.makedirs(dir)
+        for file in os.listdir(folderLoc):
+            if(file.endswith(".csv")):
+                fileName = file[:-14]
+                #make folder
+                f = os.path.join(folderLoc, file)
+                processList = []
+                processList.append("python3 ../../../../pythonScripts/plotCwnd.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotU.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotUsmoothed.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotAdditiveIncrease.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotRtt.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotAvgRtt.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotEstimatedRtt.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotThroughput.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotPathChangeQueueLength.py ../../../" + folderLoc + file)
+                processList.append("python3 ../../../../pythonScripts/plotPathChangeQueueingTime.py ../../../" + folderLoc + file)
+                processNestedList.append(processList)
+                processFolderNameList.append(fileName)
+                
+                for fil in folders:
+                    if fil in fileName:
+                        dir2 = '../pythonResults/' + arg + "/" + fil + "/" + fileName
+                        if os.path.exists(dir2):
+                            shutil.rmtree(dir2)
+                        os.makedirs(dir2)
+        currentProc = 0
+        currProcList = []
+        print("Generating python results for Run #" + str(currentProc) + ")")
+        currentFolderIter = 0
+        for procList in processNestedList:
+            for proc in procList:
+                foldName = "Other"
+                for folderName in folders:
+                    if folderName in processFolderNameList[currentFolderIter]:
+                        foldName = folderName
+                currProcList.append(subprocess.Popen(proc , shell=True, cwd='../pythonResults/' + arg + "/" + foldName + "/" + processFolderNameList[currentFolderIter]))
+                currentProc = currentProc + 1
+                print("Generating plot " + proc + " for CSV file " + processFolderNameList[currentFolderIter] + " ... (Run #" + str(currentProc) + ")")
+                if(currentProc == cores):
+                    procCompleteNum = 0
+                    for p in currProcList:
+                        p.wait()
+                        procCompleteNum = procCompleteNum + 1
+                        print("\tPlot process #" + str(procCompleteNum) + " is complete!")
+                    print("     ... Running next batch! ...\n")     
+                    currentProc = 0
+                    currProcList.clear() 
+            currentFolderIter = currentFolderIter + 1
+        processNestedList.clear()
+        processFolderNameList.clear()
+        
+        
